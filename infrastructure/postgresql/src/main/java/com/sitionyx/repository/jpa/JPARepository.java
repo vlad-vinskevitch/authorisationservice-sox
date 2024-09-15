01190af5a1d4befcaffa6d76216f9c130fc9e768
m83city <rostorguievv@gmail.com>
2024-09-15 14:47:03 +0300

    upd

--- infrastructure/postgresql/src/main/java/com/sitionyx/repository/jpa/JPARepository.java
+++
@@ -1,7 +0,0 @@
-package com.sitionyx.repository.jpa;
-
-import com.sitionyx.repository.entity.StudentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JPARepository extends JpaRepository<StudentEntity, Long> {
-}
